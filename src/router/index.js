5bfdb44fd208f6b21829926a734f3ab2c22bdb84
Kirill <kirilkanote@gmail.com>
2025-11-23 13:15:33 +0300

    add source page

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '../views/authpage.vue'
 import MainPage from '../views/mainpage.vue'
+import SourcePage from '../views/source.vue'
 
 const routes = [
   { path: '/', component: HomePage },
   { path: '/main', component: MainPage},
+  { path: '/source', component: SourcePage},
 ]
 
 const router = createRouter({
--- src/views/mainpage.vue
+++ src/views/mainpage.vue
@@ -1,8 +1,17 @@
 <template>
     <div class="header">
         <header>
-
+            <div class="navigation">
+            <nav>
+                <router-link to="/main">Главная</router-link>
+                <router-link to="/source">Источники данных</router-link>
+                <router-link to="/dashboard">Дашборды</router-link>
+            </nav>
+            </div>
         </header>
+    </div>
+    <div class="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
+        <router-view />
     </div>
     <div class="footer">
         <footer>
