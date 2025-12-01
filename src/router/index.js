d8ca05dba1311f9301ca4d66f0c8c789b2717ef4
Kirill <kirilkanote@gmail.com>
2025-12-01 22:49:40 +0300

    add protocols

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,15 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '../views/authpage.vue'
 import MainPage from '../views/mainpage.vue'
 import SourcePage from '../views/sourcepage.vue'
+import FkForm from '../views/fkform.vue'
+import Protocols from '../views/protocols.vue'
 
 const routes = [
   { path: '/', component: HomePage },
   { path: '/main', component: MainPage},
   { path: '/main/sourcepage', component: SourcePage},
+  { path: '/main/fkform', component: FkForm},
+  { path: '/main/protocols', component: Protocols},
 ]
 
 const router = createRouter({
